30122af0a73a2580994e68732212740979b3aa70
unknown <tejashxworkz9gmail.com>
2024-08-23 09:53:58 +0530

    tasks

---
+++ tej/xworkz/src/com/xworkz/engineering/runner/SideDishStore.java
@@ -0,0 +1,30 @@
+package com.xworkz.engineering.runner;
+import com.xworkz.engineering.data.SideDishData;
+
+public class SideDishStore {
+	private SideDishData[] datas = new SideDishData[5];
+	private int index=0;
+	
+	public void save(SideDishData data)
+	{
+		System.out.println("Create a Side Dish..............");
+		this.datas[this.index]=data;
+		index++;
+		
+	}
+	
+	public void display()
+	{
+		System.out.println("Side dish display..............");
+		for(SideDishData data:datas)
+		{
+			if(data!=null) {
+				data.shows();
+			}
+			else {
+				System.out.println("Data is null");
+			}
+		}
+	}
+
+}
---
+++ tej/xworkz/src/com/xworkz/engineering/store/SandleRunner.java
@@ -0,0 +1,38 @@
+package com.xworkz.engineering.store;
+import  com.xworkz.engineering.data.SandleData;
+import com.xworkz.engineering.runner.SandleStore;
+
+public class SandleRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		SandleStore sandleStore=new SandleStore();
+		sandleStore.details();
+		
+		
+		SandleData SandleData1=new SandleData("Bata", 'M', 500, "Black");
+		sandleStore.save(SandleData1);
+		sandleStore.details();
+		
+		SandleData SandleData2=new SandleData("Crocs", 'L', 1000, "Grey");
+		sandleStore.save(SandleData2);
+		sandleStore.details();
+		
+		SandleData SandleData3=new SandleData("Woodland", 's', 5000, "Brown");
+		sandleStore.save(SandleData3);
+		sandleStore.details();
+		
+		SandleData SandleData4=new SandleData("Liberty", 'M', 800, "Pink");
+		sandleStore.save(SandleData4);
+		sandleStore.details();
+		
+		SandleData SandleData5=new SandleData("Nike", 'X', 1000, "Black");
+		sandleStore.save(SandleData5);
+		sandleStore.details();
+		
+		
+	}
+
+}
