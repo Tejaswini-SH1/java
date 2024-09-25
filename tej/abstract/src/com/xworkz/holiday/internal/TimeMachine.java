23629149c6b0ea4b15f9ec6a75f53a66c456d2e3
unknown <tejashxworkz9@gmail.com>
2024-09-25 22:01:35 +0530

    Abstract

---
+++ tej/abstract/src/com/xworkz/holiday/internal/TimeMachine.java
@@ -0,0 +1,13 @@
+package com.xworkz.holiday.internal;
+
+public abstract class TimeMachine { 
+	public TimeMachine() {
+		System.out.println("no-arg const of TimeMachine");
+	}
+	
+	public abstract void TravelPast();
+	
+	
+	}
+
+
