b5aa1e3a517d28367ccd2c07e50dc9e386d0c574
unknown <tejashxworkz9@gmail.com>
2024-10-21 23:25:10 +0530

    oldfile

---
+++ tej/Tejaswini/src/com/xworkz/Tejaswini/runner/ParkRunner.java
@@ -0,0 +1,16 @@
+package com.xworkz.Tejaswini.runner;
+
+import com.xworkz.Tejaswini.things.CubbonPark;
+
+public class ParkRunner {
+
+	public static void main(String[] args) {
+		
+			
+			CubbonPark cubbonPark1=new CubbonPark();
+			cubbonPark1.open();
+			cubbonPark1.close();
+
+	}
+
+}
---
+++ tej/Tejaswini/src/com/xworkz/Tejaswini/runner/ShoeRunner.java
@@ -0,0 +1,17 @@
+package com.xworkz.Tejaswini.runner;
+
+import com.xworkz.Tejaswini.things.Shoe;
+
+public class ShoeRunner {
+
+		public static void main(String[] args) {
+			
+			Shoe shoe=new Shoe("Nike", "Black", 1500, 32);
+			shoe.dispaly();
+			
+						
+			
+		}
+
+	}
+
